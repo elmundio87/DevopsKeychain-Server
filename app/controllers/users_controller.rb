fe70671ea6315b13a425bb81188512af49fec484
Edmund Dipple <elmundio1987@gmail.com>
2014-05-13 21:18:13 +0100

    generate private keys for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,22 @@ end
 def update
 	
 	user = User.find_by(username: params[:user][:username])
-	user.update(pub_key: "rofl")
-	redirect_to users_path
+	user.update(pub_key: generate_key())
 
+end
+
+def generate_key()
+		require 'openssl'
+		rsa_key = OpenSSL::PKey::RSA.new(2048)
+
+		cipher =  OpenSSL::Cipher::Cipher.new('des3')
+
+		private_key = rsa_key.to_pem(cipher,'password')
+		public_key = rsa_key.public_key.to_pem
+		key_pair = private_key + public_key
+
+		send_data private_key, :filename => 'private.pem'
+		return public_key
 end
 
 
