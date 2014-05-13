626fc2cfbda11bdfdf8f4d10525724ecef4f5380
Edmund Dipple <elmundio1987@gmail.com>
2014-05-13 19:33:07 +0100

    generate private keys through the interface

--- app/controllers/privatekeys_controller.rb
+++ app/controllers/privatekeys_controller.rb
@@ -1,2 +1,21 @@
 class PrivatekeysController < ApplicationController
+
+	def new
+		render html: "privatekeys.html"
+	end
+
+	def create
+		require 'openssl'
+		rsa_key = OpenSSL::PKey::RSA.new(2048)
+
+		cipher =  OpenSSL::Cipher::Cipher.new('des3')
+
+		private_key = rsa_key.to_pem(cipher,'password')
+		public_key = rsa_key.public_key.to_pem
+		key_pair = private_key + public_key
+
+		send_data private_key, :filename => 'private.pem'
+		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ DevopsKeychain::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     get 'key' => 'privatekeys#new'
+     get 'key/create' => 'privatekeys#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
