bcde120a093dbd3efb8bd37a31f8d6265f46cc9e
Edmund Dipple <elmundio1987@gmail.com>
2014-05-13 18:55:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevopsKeychain::Application.config.secret_key_base = '6c8ae34d98ebfae155d7bf9f55219657a02329aaf71d6bf2a185846b6bbf5b5f4c733af2b027a79ea2f51ee25430dd195c7c940f9c5fd7cadc918874649733a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevopsKeychain::Application.config.session_store :cookie_store, key: '_DevopsKeychain_session'
