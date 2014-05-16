24a2edc848dd3fd1be724e268eb5f104fac6bea9
Edmund Dipple <elmundio1987@gmail.com>
2014-05-16 12:06:03 +0100

    refactor the encryption methods for easy unit testing

--- app/controllers/secure_content_controller.rb
+++ app/controllers/secure_content_controller.rb
@@ -2,19 +2,40 @@ class SecureContentController < ApplicationController
 
 def get
 
+	user = User.find_by(email: params[:email])
+	content = SecureContent.find_by(name: params[:securecontent])[:content]
+	public_key = user[:pub_key]
+	
+	send_data public_encrypt(public_key,content)
+
+end
+
+
+
+def public_encrypt(key, plaintext_string)
 
 	require 'openssl'
 	require 'base64'
 
-	user = User.find_by(email: params[:email])
-	content = SecureContent.find_by(name: params[:securecontent])[:content]
-
-	public_key_file = Base64.decode64(user[:pub_key]);
+	public_key_file = Base64.decode64(key);
 	
 	public_key = OpenSSL::PKey::RSA.new(public_key_file)
-	encrypted_string = Base64.encode64(public_key.public_encrypt(content))
+	encrypted_string = Base64.encode64(public_key.public_encrypt(plaintext_string))
 
-	send_data encrypted_string
+	return encrypted_string
+
+end
+
+def private_decrypt (key, encrypted_string)
+
+	require 'openssl'
+	require 'base64'
+
+	password = 'password'
+	private_key = OpenSSL::PKey::RSA.new(key,password)
+
+	return private_key.private_decrypt(Base64.decode64(encrypted_string))
+
 end
 
 
