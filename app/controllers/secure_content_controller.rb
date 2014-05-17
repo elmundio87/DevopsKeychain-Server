e5b90a3f80a7bc528c82e02444dad57b5308ccd4
Edmund Dipple <elmundio1987@gmail.com>
2014-05-17 22:45:44 +0100

    checks for the auth key before passing back data

--- app/controllers/secure_content_controller.rb
+++ app/controllers/secure_content_controller.rb
@@ -1,30 +1,58 @@
 class SecureContentController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
 def get
 
 	user = User.find_by(email: params[:email])
 	content = SecureContent.find_by(name: params[:securecontent])[:content]
 	public_key = user[:pub_key]
-	
-	send_data public_encrypt(public_key,content)
+
+  keychain_auth = params[:auth]
+
+  if(keychain_auth.blank?)
+    render :file => "public/500", :status => :unauthorized
+    return
+  end
+
+
+  if( user[:email] == public_decrypt(public_key,keychain_auth))
+      send_data public_encrypt(public_key,content)
+  else
+    render :file => "public/401", :status => :unauthorized
+  end
 
 end
-
-
 
 def public_encrypt(key, plaintext_string)
 
 	require 'openssl'
 	require 'base64'
 
-	public_key_file = Base64.decode64(key);
-	
+	public_key_file = Base64.decode64(key)
+
 	public_key = OpenSSL::PKey::RSA.new(public_key_file)
 	encrypted_string = Base64.encode64(public_key.public_encrypt(plaintext_string))
 
 	return encrypted_string
 
 end
+
+def private_encrypt(key, plaintext_string)
+
+    require 'openssl'
+    require 'base64'
+
+    private_key_file = Base64.decode64(key)
+
+    password = "password"
+    private_key = OpenSSL::PKey::RSA.new(public_key_file,password)
+    encrypted_string = Base64.encode64(private_key.private_encrypt(plaintext_string))
+
+    return encrypted_string
+
+  end
+
 
 def private_decrypt (key, encrypted_string)
 
@@ -37,6 +65,21 @@ def private_decrypt (key, encrypted_string)
 	return private_key.private_decrypt(Base64.decode64(encrypted_string))
 
 end
+
+def public_decrypt (key, encrypted_string)
+
+  require 'openssl'
+  require 'base64'
+
+  public_key_file = Base64.decode64(key)
+  public_key = OpenSSL::PKey::RSA.new(public_key_file)
+  encrypted_string = Base64.decode64(encrypted_string)
+
+  return public_key.public_decrypt(encrypted_string)
+
+end
+
+
 
 
 end
