27e02154a6c3d6f601c928eff0cd1c9556229424
Edmund Dipple <elmundio1987@gmail.com>
2014-06-04 07:50:16 +0100

    install nginx default website + set up database for development

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install postgresql-9.1 libpq-dev nodejs openssl build-essential curl -y
+sudo apt-get install postgresql-9.1 libpq-dev nodejs openssl build-essential curl nginx -y
 sudo apt-get --purge remove ruby-rvm
 sudo rm -rf /usr/share/ruby-rvm /etc/rvmrc /etc/profile.d/rvm.sh
 
@@ -16,3 +16,13 @@ sudo gem install rails  --no-ri --no-rdoc
 sudo gem install bundler  --no-ri --no-rdoc
 
 popd
+
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+export PGPASSWORD=postgres
+createuser --superuser -U postgres -h localhost dev
+
+sudo -u postgres psql -c "ALTER USER dev PASSWORD 'password1';"
+
+cp /vagrant/nginx/default /etc/nginx/sites-available/default
+service nginx restart
